7a959ff3f13c3be2624c48640b84913cd999e4c0
dcarorod <22807958+dcarorod@users.noreply.github.com>
2024-10-10 15:28:49 +0200

    restored ft_realloc to previous version and added safe malloc, safe memresize and safe realloc

--- libs/libft/include/ft_memory.h
+++ libs/libft/include/ft_memory.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memory.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
+/*   By: dcaro-ro <dcaro-ro@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 13:56:13 by dcaro-ro          #+#    #+#             */
-/*   Updated: 2024/10/09 17:32:04 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/10/10 15:25:42 by dcaro-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,13 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-void	*ft_realloc(void *ptr, size_t new_size, size_t old_size);
+void	*ft_realloc(void *ptr, size_t size);
 void	*ft_safe_free(unsigned int num_ptrs, ...);
 void	*ft_memresize(void *ptr, size_t old_size, size_t new_size);
 void	*ft_free(void *ptr);
+void	*ft_safe_malloc(size_t size, char *error_message);
+void	*ft_safe_memresize(void *ptr, size_t old_size,
+			size_t new_size, char *error_message);
+void	*ft_safe_realloc(void *ptr, size_t size, char *error_message);
 
 #endif
