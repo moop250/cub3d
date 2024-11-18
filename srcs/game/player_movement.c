a9f2e9c06bbd77709cff86b5f87cdd6856015599
moop250 <hugo.libine@proton.me>
2024-11-18 14:51:14 +0100

    fixed out of bounds  seg fault this seg would happen if a corner was not placed in the map and the player tried to clip through it

--- srcs/game/player_movement.c
+++ srcs/game/player_movement.c
@@ -6,10 +6,9 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2024/11/15 21:06:14 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/11/18 14:49:18 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "../../include/cub3d.h"
 
@@ -20,11 +19,14 @@ static void	collision(t_game *game, double new_x, double new_y, t_player *play)
 
 	map_x = (int)(new_x);
 	map_y = (int)(new_y);
-	if (game->mapdata->map[map_y][map_x] != '1')
+	if (game->mapdata->map[map_y][map_x] != '1'
+		&& game->mapdata->map[map_y][map_x] != ' ')
 		play->pos = (t_vector){new_x, new_y};
-	else if (game->mapdata->map[(int)play->pos.y][map_x] != '1')
+	else if (game->mapdata->map[(int)play->pos.y][map_x] != '1'
+		&& game->mapdata->map[(int)play->pos.y][map_x] != ' ')
 		play->pos = (t_vector){new_x, play->pos.y};
-	else if (game->mapdata->map[map_y][(int)play->pos.x] != '1')
+	else if (game->mapdata->map[map_y][(int)play->pos.x] != '1'
+		&& game->mapdata->map[map_y][(int)play->pos.x] != ' ')
 		play->pos = (t_vector){play->pos.x, new_y};
 }
 
