fd4b8389cc5aa4f803f585ab16eaa4d5977325ba
moop250 <hugo.libine@proton.me>
2024-11-19 15:50:23 +0100

    changed player movement so they cant get too close to walls

--- srcs/game/player_movement.c
+++ srcs/game/player_movement.c
@@ -6,28 +6,35 @@
 /*   By: hlibine <hlibine@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2024/11/19 14:52:02 by hlibine          ###   ########.fr       */
+/*   Updated: 2024/11/19 15:35:23 by hlibine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-static void	collision(t_game *game, double new_x, double new_y, t_player *play)
+static void	collision(t_game *game, double *new_x_y, double *step_sizes)
 {
-	int		map_x;
-	int		map_y;
+	int			map_x;
+	int			map_y;
+	double		check_x_y[2];
+	t_player	*player;
 
-	map_x = (int)(new_x);
-	map_y = (int)(new_y);
+	player = &game->mapdata->player;
+	check_x_y[0] = player->pos.x + (player->dir.x * (step_sizes[0] * MIN_DIST))
+		- (player->dir.y * (step_sizes[1] * MIN_DIST));
+	check_x_y[1] = player->pos.y + (player->dir.y * (step_sizes[0] * MIN_DIST))
+		+ (player->dir.x * (step_sizes[1] * MIN_DIST));
+	map_x = (int)(check_x_y[0]);
+	map_y = (int)(check_x_y[1]);
 	if (game->mapdata->map[map_y][map_x] != '1'
 		&& game->mapdata->map[map_y][map_x] != ' ')
-		play->pos = (t_vector){new_x, new_y};
-	else if (game->mapdata->map[(int)play->pos.y][map_x] != '1'
-		&& game->mapdata->map[(int)play->pos.y][map_x] != ' ')
-		play->pos = (t_vector){new_x, play->pos.y};
-	else if (game->mapdata->map[map_y][(int)play->pos.x] != '1'
-		&& game->mapdata->map[map_y][(int)play->pos.x] != ' ')
-		play->pos = (t_vector){play->pos.x, new_y};
+		player->pos = (t_vector){new_x_y[0], new_x_y[1]};
+	else if (game->mapdata->map[(int)player->pos.y][map_x] != '1'
+		&& game->mapdata->map[(int)player->pos.y][map_x] != ' ')
+		player->pos = (t_vector){new_x_y[0], player->pos.y};
+	else if (game->mapdata->map[map_y][(int)player->pos.x] != '1'
+		&& game->mapdata->map[map_y][(int)player->pos.x] != ' ')
+		player->pos = (t_vector){player->pos.x, new_x_y[1]};
 }
 
 /*
@@ -41,8 +48,7 @@ void	move_player(t_game *game, t_move dir)
 	t_player	*player;
 	double		move_step;
 	double		strafe_step;
-	double		new_x;
-	double		new_y;
+	double		new_x_y[2];
 
 	move_step = 0;
 	strafe_step = 0;
@@ -57,11 +63,11 @@ void	move_player(t_game *game, t_move dir)
 		strafe_step = -MOVE_SPEED;
 	else
 		return ;
-	new_x = player->pos.x + (player->dir.x * move_step)
+	new_x_y[0] = player->pos.x + (player->dir.x * move_step)
 		- (player->dir.y * strafe_step);
-	new_y = player->pos.y + (player->dir.y * move_step)
+	new_x_y[1] = player->pos.y + (player->dir.y * move_step)
 		+ (player->dir.x * strafe_step);
-	collision(game, new_x, new_y, player);
+	collision(game, new_x_y, (double[]){move_step, strafe_step});
 }
 
 /*
